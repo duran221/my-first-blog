24703b0f6f13daecde65c3b50a87c278b3d2d477
Cristian <cristianfernandoduran@gmail.com>
2018-12-06 13:24:01 -0500

    Mi aplicacion Django. mi primer commit

---
+++ miblog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+#Importando la clase (objeto Publicacion):
+
+from .models import Publicacion
+
+# Register your models here.
+
+
+# Para hacer nuestro modelo visible en la página del administrador, tenemos que registrar el modelo con 
+admin.site.register(Publicacion)
+
---
+++ miblog/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+#importando la zona horaria de django
+
+from django.utils import timezone
+
+# Create your models here.
+
+
+#esta línea define nuestro modelo (es un objeto). CLASE
+#models.Model significa que Publicacion es un modelo de Django, así Django sabe que debe guardarlo en la base de datos.
+class Publicacion(models.Model):
+
+    #models.CharField, así es como defines un texto con un número limitado de caracteres.
+    titulo= models.CharField(max_length=200)
+
+    #este es una relación (link) con otro modelo. (Llave foranea)
+    autor= models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+    #este es para texto largo sin límite. Suena perfecto para el contenido de la entrada del blog, ¿no?
+    texto = models.TextField()
+
+    #este es fecha y hora.
+    fecha_creacion= models.DateField(default=timezone.now())
+
+    #null-True: se permiten valores nulos en la tabla de Database
+    fecha_publicacion= models.DateField(blank=True,null=True)
+
+
+    """
+    Metodo permite realizar una publicacion
+    
+    """
+    def publicar(self):
+
+        self.fecha_publicacion= timezone.now()
+
+        self.save()
+
+
+    """
+    Retornar el titulo de la publicacion
+    """
+    def __str__(self):
+
+        return self.titulo
+
+
+
+
+
