5fcec4549f0408ace436f9cf5d5493bc8823d302
Cristian <cristianfernandoduran@gmail.com>
2018-12-08 22:23:43 -0500

    Agregando vistas y plantilla para el detalle del post del blog asi como tambien CSS para el sitio

--- miblog/urls.py
+++ miblog/urls.py
@@ -10,4 +10,11 @@ from . import views
 
 #Segundo parametro: Asociando una vista (view) llamada post_list a la URL raíz
 #Tercer Parametro:  name='post_list' es el nombre de la URL que se utilizará para identificar a la vista.
-urlpatterns = [path('', views.lista_publicaciones,name= 'lista_publicaciones')]
+urlpatterns = [path('', views.lista_publicaciones,name= 'lista_publicaciones'),
+               path('publicacion/<int:pk>/',views.detalles_publicacion,name='detalles_publicacion')]
+            #En la anterior linea: 1.Parametro: 'publicacion/' la url deberia empezar con dicha palabra seguido de '/'
+            #int:pk/->Significa que Django buscará un número entero y se lo pasará a la vista en una variable llamada pk.
+            #Esto quiere decir que si pones http://127.0.0.1:8000/post/5/ en tu navegador, Django entenderá que estás buscando
+            #una vista llamada post_detail y transferirá la información de que pk es igual a 5 a esa vista.
+
+    
--- miblog/views.py
+++ miblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 
 from django.utils import timezone
@@ -18,6 +18,21 @@ def lista_publicaciones(request):
     #'miblog/lista_publicaciones.html': Ruta de la plantilla
     #{} :es un lugar en el que podemos agregar algunas cosas para que la plantilla las use
     return render(request,'miblog/lista_publicaciones.html',{'publicaciones':publicaciones})
+
+
+
+"""
+    permite retornar una vista hacia la direccion 'miblog/detalles_publicacion.html'
+    request:
+    pk: Llave primaria o numero consecutivo de la base de datos
+"""
+def detalles_publicacion(request,pk):
+
+        publicacion= get_object_or_404(Publicacion,pk=pk)
+        return render(request,'miblog/detalles_publicacion.html',{'publicacion':publicacion})
+
+
+
 
 
 
